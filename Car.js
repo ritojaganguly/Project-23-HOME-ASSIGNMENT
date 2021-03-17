4ba0557eb39bd4fcbc6172805c4840b78b190eb7
ritojaganguly <78806021+ritojaganguly@users.noreply.github.com>
2021-03-17 12:25:20 +0530

    Update Car.js

--- Car.js
+++ Car.js
@@ -4,7 +4,7 @@ class Car{
         this.y = height-random([150,250,350,950,1050,1150,1750,1850,1950]);
         this.spt = createSprite(this.x,this.y);
         this.spt.shapeColor = "blue";
-        this.spt.scale=0.5;
+        this.spt.scale=0.01;
         this.spt.addAnimation("car", carAnimation1);
 
         if(this.y==height-250 || this.y==height-1050 || this.y==height-1850)
